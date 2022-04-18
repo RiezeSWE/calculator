1b548570b62197361c502b797289f6ae9dc94315
RiezeSWE <riezenfell@outlook.com>
2022-04-18 12:43:35 +0800

    Add button IDs

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//needs to be able to
+//need to make a clear button that will take any input currently on the screen and reset it to zero
+//delete button - clears the last integer or operand from the screen
+//equals button - tallies result of the values
